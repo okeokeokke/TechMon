472b49aec38d06d541680a74c60b9f908e8f5e49
Aika Okemoto <aika2004oke@gmail.com>
2020-07-31 20:09:54 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 桶本あいか on 2020/07/31.
+//  Copyright © 2020 Okemoto.Okke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
